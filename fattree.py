863b9cc0016331135893f7fd5743701b3b3a952c
Jacobo Rdz <jacoborzt@gmail.com>
2026-07-04 00:59:19 -0600

    fix: remove the old use of reset on ins and act

--- fattree.py
+++ fattree.py
@@ -17,14 +17,10 @@ act = Action()
 
 # Install the ARP
 for i, dpid in enumerate(nodes):
-    ins.reset()
-    act.reset()
     c.setFlow(dpid, Flow(f"{i}000", "arp", table, 100, Match.arp(),[ins.apply([act.output("NORMAL")])]))
 
 # Install the core distribution on S1
 for i in range(1, 3):
-    ins.reset()
-    act.reset()
     c.setFlow(core,
         Flow(
             id=f"10{i}",
@@ -40,40 +36,25 @@ for i in range(1, 3):
         )
     )
 
-ins.reset()
-act.reset()
-
 c.setFlow(aggrs[0], Flow("201", "aggr1-to-edge1", table, 100,
     Match.eth(dst_ip="10.0.1.0/24"),
     [ ins.apply([ act.output("3") ]) ]
 ))
-
-ins.reset()
-act.reset()
 
 c.setFlow(aggrs[0], Flow("202", "aggr1-to-core", table, 100,
     Match.eth(dst_ip="10.0.2.0/24"),
     [ ins.apply([ act.output("2") ]) ]
 ))
 
-ins.reset()
-act.reset()
-
 c.setFlow(aggrs[1], Flow("301", "aggr2-to-core", table, 100,
     Match.eth(dst_ip="10.0.1.0/24"),
     [ ins.apply([ act.output("2") ]) ]
 ))
 
-ins.reset()
-act.reset()
-
 c.setFlow(aggrs[1], Flow("302", "aggr2-to-edge2", table, 100,
     Match.eth(dst_ip="10.0.2.0/24"),
     [ ins.apply([ act.output("3") ]) ]
 ))
-
-ins.reset()
-act.reset()
 
 # Edge Flows
 for i, edge in enumerate(edges, start=1):
@@ -82,20 +63,14 @@ for i, edge in enumerate(edges, start=1):
         Match.eth(dst_ip="10.0.0.0/16"),
         [ ins.apply([ act.output(2) ]) ]
     ))
-    ins.reset()
-    act.reset()
     # host distrubution
     for host in range(1, n_host+1):
-        ins.reset()
-        act.reset()
         c.setFlow(edge, Flow(f"{i}0{host}", f"{i}-to-host{host}", table, 100,
             Match.eth(dst_ip=f"10.0.{i}.{host}/32"),
             [ ins.apply([ act.output(host + 12) ]) ]
         ))
 
 for host in range(5, 7):
-    ins.reset()
-    act.reset()
     c.setFlow(edges[-1], Flow(f"50{host}", f"host-{host}", table, 100,
         Match.eth(dst_ip=f"10.0.2.{host}/32"),
         [ ins.apply([ act.output(host + 12) ]) ]
